353af76ff6800c315bb7a041bec4755ffd8722c1
Alex Afeltra <afeltra.alex@gmail.com>
2016-12-17 02:41:23 +0000

    added profile and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   belongs_to :plan
+  has_one :profile
   
   attr_accessor :stripe_card_token
   #if pro user passes validation(email, password etc..),
