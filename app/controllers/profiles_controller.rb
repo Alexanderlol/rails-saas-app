70da5c3374ce812112d17362be8f143ba96ac922
Alex Afeltra <afeltra.alex@gmail.com>
2016-12-18 03:38:05 +0000

    added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
   #GET to /users/:user_id/profile/new
   def new
-    
+    #render a blank profile details form
+    @profile = Profile.new
   end
 end
